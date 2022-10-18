00bfb4a50bfa3f2a3949b86fc4254219e5204538
sofiotchighitashvili@gmail.com <sofiotchighitashvili@gmail.com>
2022-10-18 19:36:01 +0400

    Initial Commit

---
+++ Solutions/1700-1800/1791-FindCenterofStarGraph.cs
@@ -0,0 +1,15 @@
+﻿namespace Solutions._1700_1800
+{
+    public class _1791_FindCenterofStarGraph
+    {
+        public int FindCenter(int[][] edges)
+        {
+            if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+            {
+                return edges[0][0];
+            }
+
+            return edges[0][1];
+        }
+    }
+}

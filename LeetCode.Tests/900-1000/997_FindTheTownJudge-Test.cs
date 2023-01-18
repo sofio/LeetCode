5f981b264ee1fd3196db40dfa9b0fddc56c96d9a
sofiotchighitashvili@gmail.com <sofiotchighitashvili@gmail.com>
2023-01-18 17:40:53 +0400

    997_Find the Town Judge

--- LeetCode.Tests/900-1000/997_FindTheTownJudge-Test.cs
+++ LeetCode.Tests/900-1000/997_FindTheTownJudge-Test.cs
@@ -1,20 +1,24 @@
 ﻿using NUnit.Framework;
 using Solutions._900_1000;
-
 namespace LeetCode.Tests._900_1000
 {
     public class _997_FindTheTownJudge_Test
     {
-        [Test]
-        public void FindTheTownJudge()
-        {
-            var trust = new int[][] { new int[] { 1, 2 }, new int[] { 2, 1 } };
+        static readonly object[] FindTheTownJudgeTestCaseData =
+            {
+                new object[] { new int[][] { new int[] { 1, 3 }, new int[] { 1, 4 }, new int[] { 2, 3 }, new int[] { 4, 3 } }, 4, 3 },
+                new object[] { new int[][] { new int[] { 1 } }, 1, 1 },
+                new object[] { new int[][] { new int[] { 1, 2 }, new int[] { 2, 1 } }, 2, -1 },
+                new object[] { new int[][] { new int[] { 1, 3 }, new int[] { 2, 3 }, new int[] { 3, 1 } }, 3, -1 }
+            };
 
+        [Test]
+        [TestCaseSource(nameof(FindTheTownJudgeTestCaseData))]
+        public void FindTheTownJudge(int[][] trust, int n, int expectedResult)
+        {
             var solution = new _997_FindTheTownJudge();
-            var result = solution.FindJudge(2,trust);
-            Assert.That(result, Is.EqualTo(-1));
+            var result = solution.FindJudge(n, trust);
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
-
-//[[1,3],[1,4],[2,3],[2,4],[4,3]]
--- Solutions/900-1000/997_FindTheTownJudge.cs
+++ Solutions/900-1000/997_FindTheTownJudge.cs
@@ -6,13 +6,23 @@ namespace Solutions._900_1000
     {
         public int FindJudge(int n, int[][] trust)
         {
+            if (n == 1)
+            {
+                return 1;
+            }
+            if (trust.Length == 0)
+            {
+                return -1;
+            }
 
             List<int> possibleJudges = new List<int>();
             possibleJudges.Add(trust[0][1]);
 
-            int trustedCount = 0;
-            int judge = 0;
-            int judgesCount = 0;
+            List<int> trustGivers = new List<int>();
+            trustGivers.Add(trust[0][0]);
+
+
+
 
             for (int i = 1; i < trust.Length; i++)
             {
@@ -20,71 +30,55 @@ namespace Solutions._900_1000
                 {
                     possibleJudges.Add(trust[i][1]);
                 }
+
+                if (!trustGivers.Contains(trust[i][0]))
+                {
+                    trustGivers.Add(trust[i][0]);
+                }
             }
+
+            for (int j = 0; j < possibleJudges.Count; j++)
+            {
+                for (int i = 0; i < trustGivers.Count; i++)
+                {
+                    if (possibleJudges.Contains(trustGivers[i]))
+                    {
+                        possibleJudges.Remove(trustGivers[i]);
+                        j--;
+                    }
+                }
+            }
+
+            int judge = -1;
+            int judgesCount = 0;
+            int trustedCount = 0;
 
             for (int j = 0; j < possibleJudges.Count; j++)
             {
                 for (int i = 0; i < trust.Length; i++)
                 {
-
-                    if (trust[i][0] == possibleJudges[j] && j != possibleJudges.Count-1)
+                    if (trust[i][1] == possibleJudges[j])
                     {
-                        i = 0;
-                        j++;
-                        trustedCount = 0;
-                    }
-                    else if (trust[i][1] == possibleJudges[j])
                         trustedCount++;
-                    if (i == trust.Length-1 && trustedCount == n - 1)
-                    {
-                        judge = possibleJudges[j];
-                        judgesCount++;
-                        trustedCount = 0;
                     }
                 }
-
-                
+                if (trustedCount == n - 1)
+                {
+                    judge = possibleJudges[j];
+                    judgesCount++;
+                }
+                trustedCount = 0;
             }
+
 
             if (judgesCount == 1)
             {
                 return judge;
             }
             else
-                return -1;
-
-
-            /*for (int j = 0; j < possibleJudges.Count; j++)
             {
-                for (int i = 0; i < trust.Length; i++)
-                {
-
-                    if (trust[i][0] == possibleJudges[j])
-                    {
-                        possibleJudges.Remove(possibleJudges[j]);
-                        if (possibleJudges.Count == 0)
-                            return -1;
-                    }
-                    else if (trust[i][1] == possibleJudges[j])
-                        trustedCount++;
-                }
-
-                if (trustedCount != n - 1)
-                {
-                    possibleJudges.Remove(possibleJudges[j]);
-                    if (possibleJudges.Count == 0)
-                        return -1;
-                }
-            }
-
-            if (possibleJudges.Count == 1)
-            {
-                return possibleJudges[0];
-            }
-            else
                 return -1;
-        }*/
-
+            }
         }
     }
 }

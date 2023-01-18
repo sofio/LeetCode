6b81d07cbb09b851606eba4b2c9de04986ce9d25
sofiotchighitashvili@gmail.com <sofiotchighitashvili@gmail.com>
2023-01-18 22:24:30 +0400

    1-Two Sum

---
+++ LeetCode.Tests/0-100/01-TwoSum-Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Solutions._0_100;
+
+namespace LeetCode.Tests._0_100
+{
+    public class _01_TwoSum_Test
+    {
+        [Test]
+        [TestCase(new int[4] { 2, 7, 11, 15 }, 9, new int[2] { 0, 1 })]
+        [TestCase(new int[3] { 3, 2, 4 }, 6, new int[2] { 1, 2 })]
+        [TestCase(new int[2] { 4, 4 }, 8, new int[2] { 0, 1 })]
+
+        public void TwoSumTest(int[] nums, int targets, int[] expectedResult)
+        {
+            var solution = new _01_TwoSum();
+            var result = solution.TwoSum(nums, targets);
+            CollectionAssert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ Solutions/0-100/01-TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solutions._0_100
+{
+    public class _01_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var valuePairs = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (valuePairs.ContainsKey(nums[i]))
+                {
+                    return new int[] { valuePairs[nums[i]], i };
+                }
+                else
+                {
+                    valuePairs[target - nums[i]] = i;
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
